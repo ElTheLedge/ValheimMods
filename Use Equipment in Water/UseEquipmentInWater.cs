e949b21ec0bbad0d322e70cb069b074771ceeb89
G4mer1808 <g4mer1808@gmail.com>
2021-02-25 00:43:37 +0100

    Add project files.

---
+++ Use Equipment in Water/UseEquipmentInWater.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Use_Equipment_in_Water
+{
+    [BepInPlugin("com.lvh-it.valheim.useequipmentinwater", "Use Equipment in Water", "0.1.0.0")]
+    [BepInProcess("valheim.exe")]
+    class UseEquipmentInWater : BaseUnityPlugin
+    {
+        void Main()
+        {
+            Harmony.CreateAndPatchAll(typeof(UseEquipmentInWater));
+        }
+
+        [HarmonyPatch(typeof(Character), "IsSwiming")]
+        [HarmonyPrefix]
+        static bool patchIsSwim(Humanoid __instance, ref bool __result)
+        {
+            string callerName = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod().Name;
+            if ((callerName == "DMD<Humanoid::EquipItem>" || callerName == "UpdateEquipment") && __instance.IsPlayer())
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
